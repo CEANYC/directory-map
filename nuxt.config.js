7f4c830c1922dc97e8cfbc81f74d0cd44ba9b734
Eric Brelsford <ebrelsford@gmail.com>
2022-05-01 14:34:24 -0400

    Add command for deploying to GitHub Pages
    Related to #10

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/directory-map/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'ceanyc-directory-map',
