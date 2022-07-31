b3fffebd108dfc7021c0a78c8e929ef7f176972f
Eric Brelsford <ebrelsford@gmail.com>
2022-07-30 22:20:41 -0400

    Add sector expander

--- components/SectorSection.vue
+++ components/SectorSection.vue
@@ -1,15 +1,29 @@
 <template>
   <section class="sector">
-    <h3>{{name}}</h3>
-    <div>{{details}}</div>
-    <img v-if="imgUrl" :src="imgUrl" />
+    <h3 :class="{ name: true, expanded }" @click="toggle">{{name}}</h3>
+    <div class="content" v-if="expanded">
+      <div>{{details}}</div>
+      <img v-if="imgUrl" :src="imgUrl" />
+    </div>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+
   props: {
     sector: [Object, true],
+  },
+
+  methods: {
+    toggle() {
+      this.expanded = !this.expanded;
+    },
   },
 
   computed: {
@@ -29,9 +43,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.info-window {
-  img {
-    max-width: 100%;
+.name {
+  border: 1px solid black;
+  border-radius: 20px;
+  cursor: pointer;
+  background: #eee;
+  padding: 0.5em 2em;
+  position: relative;
+
+  &::after {
+    content: "›";
+    font-size: 2em;
+    line-height: 0;
+    position: absolute;
+    right: 1rem;
+    top: 50%;
+    transform: rotate(90deg);
   }
+
+  &.expanded {
+    &::after {
+      right: 1.5rem;
+      transform: rotate(270deg);
+    }
+  }
+}
+
+img {
+  max-width: 100%;
+}
+
+.content {
+  padding: 0 1.5em;
 }
 </style>
