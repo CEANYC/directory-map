6e60a9e639d3d5055c49163d074cab4ebfd6a90a
Eric Brelsford <ebrelsford@gmail.com>
2022-07-30 21:21:31 -0400

    Update airtable id and table

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-export const AIRTABLE_DATABASE_ID = 'appa4RhGGhuRhZfBL';
-export const AIRTABLE_TABLE = 'Directory Listings';
+export const AIRTABLE_DATABASE_ID = 'appVbmRHpF0PZbybE';
+export const AIRTABLE_TABLE = 'listings';
 
 export const MAPBOX_STYLE = 'mapbox://styles/ceanyc/cl35ym40m005f15ntsnsguiy1';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiY2VhbnljIiwiYSI6ImNsMzV5NnV2ZjM0YnQzam81NG1sazdzaDUifQ.PEE1DtXPV-YASshMbtMqEA';
