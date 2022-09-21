34b60ec7feaf85b4f32ab37d51a27e4d70d795ae
Eric Brelsford <ebrelsford@gmail.com>
2022-09-20 21:17:00 -0400

    Add mapbox-gl css

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    'mapbox-gl/dist/mapbox-gl.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
